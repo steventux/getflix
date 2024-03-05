05e10abdd5a1201265834abf52d13e49d100eb74
Steve Laing <steve.laing@gmail.com>
2024-03-05 11:35:01 +0000

    Apply some style

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -7,23 +7,22 @@ import Result from '@/app/types/result';
 
 interface ResultItemProps {
   item: Result;
-  key: number;
 }
 
-function ResultItem({item, key}: ResultItemProps) {
+function ResultItem({item}: ResultItemProps) {
   const href = item.magnet ? item.magnet : item.torrent;
   return (
-    <li key={key}>
-      (S-{item.seeders} {item.size})
-      <Link href={href}>{item.name}</Link>
+    <li>
+      (S:{item.seeders} {item.size}) <Link href={href} className="underline font-semibold">{item.name}</Link>
     </li>
   );
 }
 
 export default function SearchForm() {
+  const emptyResults = { data: [] };
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const [results, setResults] = useState({ data: [] });
+  const [results, setResults] = useState(emptyResults);
 
   const torrentApiSearchUrl: string = `${process.env.NEXT_PUBLIC_TORRENT_API_BASE_URL}/api/v1/all/search`;
   const categories: string[] = ["Movie", "Television"];
@@ -38,10 +37,17 @@ export default function SearchForm() {
 
   const search = async (event:any) => {
     event.preventDefault();
-    setLoading(true);
 
     const query = event.target.query.value; 
-    if (query.length == 0) return;
+
+    if (query.length == 0) {
+      setError('Enter something!')
+      return;
+    }
+
+    setError('');
+    setLoading(true);
+    setResults(emptyResults);
 
     const searchUrl = `${torrentApiSearchUrl}?query=${query}&limit=10`;
 
@@ -57,17 +63,25 @@ export default function SearchForm() {
 
   return (
     <>
-      <form onSubmit={search}>
-        <div className="justify-center">
-          <input type="text" name="query" placeholder="The Shining"/>
-          <button type="submit">Search</button>
+      <form onSubmit={search} className="w-3/4">
+        <div className="p-4">
+          <input type="text" name="query" placeholder="The Shining" className="w-full border-2 border-slate-500 bg-slate-50 w-3/4 p-4 mt-10"/>
+        </div>
+        { error ? (<div className="text-red-600 font-bold flex flex-col items-center">{error}</div>) : '' }
+        <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+          <button type="submit" className="bg-green-600 text-2xl font-bold text-white px-10 p-4 mx-auto">Search</button>
         </div>
       </form>
-      { loading ? (<Image src="/loading-300.gif" height={200} width={200} alt="Loading..." />) : '' }
-      { error ? (<p>{error}</p>) : '' }
+
+      { loading ? (
+        <div className="flex flex-col items-center">
+          <Image src="/loading-300.gif" height={200} width={200} alt="Loading..." />
+        </div>
+      ) : '' }
+
       <ul>
         {results.data.map((result: Result, idx) => (
-          <ResultItem item={result} key={idx}/>
+          <ResultItem item={result} key={idx} />
         ))}
       </ul>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Inter, Libre_Barcode_39_Text } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+const libre_barcode_39_text = Libre_Barcode_39_Text({ subsets: ["latin"], weight: "400" });
 import SearchForm from '@/app/components/SearchForm';
 
 export default function Home() {
   return (
-    <main className="margin-auto">
-      <div>
-        <h1>
+    <main>
+      <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className={libre_barcode_39_text.className + ' text-8xl'}>
           Getflix
         </h1>
         <SearchForm/>
