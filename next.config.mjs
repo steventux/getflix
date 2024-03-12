cbb96ea9dd5a659b63a1a2fe30435fdbb72387b0
Steve Laing <steve.laing@gmail.com>
2024-03-12 21:23:29 +0000

    Add fs fallback

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+      },
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
