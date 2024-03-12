19e8eec7436e5305ada26ea9feca876f0367c2d7
Steve Laing <steve.laing@gmail.com>
2024-03-12 21:25:09 +0000

    Add deluge libraries

---
+++ app/lib/deluge.ts
@@ -0,0 +1,24 @@
+"use server"
+
+const delugeUrl = process.env.NEXT_PUBLIC_DELUGE_URL || 'http://localhost:8112';
+const delugePath = process.env.NEXT_PUBLIC_DELUGE_PATH || 'json';
+const password = process.env.NEXT_PUBLIC_DELUGE_PASS || 'deluge';
+const deluge = require('./delugeApi')(delugeUrl, password);
+
+const connect = async () => {
+  deluge.isConnected(function (err: string, connected: boolean) {
+    if (err || !connected) {
+      deluge.getHosts(function (err: string, result: any) {
+        if (err) { console.error(err); return }
+        deluge.connect(result[0].id);
+      })
+    }
+  })
+}
+
+export default async function addTorrent(url: string) {
+  connect();
+  deluge.add(url, delugePath, (err: any, result: any) => {
+    if (err) { console.error(err); }
+  })
+}
