ddb0941f11d50f2167512cc8e9a2bb76e29e9353
Steve Laing <steve.laing@gmail.com>
2024-03-13 09:00:46 +0000

    Use local font

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import { Inter, Libre_Barcode_39_Text } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-const libre_barcode_39_text = Libre_Barcode_39_Text({ subsets: ["latin"], weight: "400" });
 import SearchForm from '@/app/components/SearchForm';
 
 export default function Home() {
   return (
     <main>
       <div className="h-screen flex flex-col items-center justify-center">
-        <h1 className={libre_barcode_39_text.className + ' text-8xl'}>
+        <h1 className="barcode text-8xl">
           Getflix
         </h1>
         <SearchForm/>
