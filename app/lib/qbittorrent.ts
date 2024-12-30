07daeda513e49a888cc88b476c624197c46f10e8
Steve Laing <steve.laing@gmail.com>
2024-12-30 19:08:12 +0000

    Pass well formed params to add torrent endpoint

--- app/lib/qbittorrent.ts
+++ app/lib/qbittorrent.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { qBittorrentClient } from '@robertklep/qbittorrent';
+import { qBittorrentClient, TorrentAddParameters } from '@robertklep/qbittorrent';
 
 const qBittorrentUrl = process.env.NEXT_PUBLIC_QBITTORRENT_URL || 'http://raspberrypi.local:8080';
 const qBittorrentUser = process.env.NEXT_PUBLIC_QBITTORRENT_USER || 'admin';
@@ -9,11 +9,11 @@ const qBittorrentPass = process.env.NEXT_PUBLIC_QBITTORRENT_PASS || '81tt0rr3nt'
 const client = new qBittorrentClient(qBittorrentUrl, qBittorrentUser, qBittorrentPass);
 
 const addTorrent = async (url: string) => {
-  await client.torrents.add(url, { seedingTimeLimit: 720 });
+  await client.torrents.add(<TorrentAddParameters>{ urls: [url], seedingTimeLimit: 720 });
 }
 
 const getTorrents = async () => {
-  const results = await client.torrents.info({ filter: 'downloading|completed' });
+  const results = await client.torrents.info({ filter: 'all' });
   return JSON.stringify(results); 
 }
 
